43d0f5be122985ad1f137a994dc45ae42461845e
jin wang <ai.ongoing@gmail.com>
2019-10-07 13:43:39 -0400

    Removed some code

--- script.py
+++ script.py
@@ -1,20 +1,6 @@
-import math
-import sys
-from os import rename
 
 import requests
 
-print(sys.version)  # tell version of python
-print(sys.executable)  # where python is located in the computer
-
-
-def greet(who_to_greet):
-    greeting = "hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Corey'))
-
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
